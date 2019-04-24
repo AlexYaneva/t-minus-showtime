b0005704a8c34345e156b01c528dc46f55ef47cf
Alex Yaneva <alexandrayaneva@yahoo.com>
2019-04-24 21:39:39 +0100

    added results template

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template, url_for, request, redirect
+import requests
+import json
 
 
 app = Flask(__name__)
@@ -10,6 +12,20 @@ app = Flask(__name__)
 @app.route('/index', methods=['GET', 'POST'])
 def index():
 	return render_template('index.html')
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+	title = request.form['title']
+	r = requests.get(f'http://www.omdbapi.com/?t={title}&apikey=b644d0fd')
+	json_obj = r.json()
+
+	title = json_obj['Title']
+	genre = json_obj['Genre']
+	cast = json_obj['Actors']
+	img = json_obj['Poster']
+	result = f'{title} {genre} {cast}'
+
+	return render_template('results.html', result=result, img=img)
 
 
 
