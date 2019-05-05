532a3de2ebd3c36772f79cb05d5af258aff71d5c
Alex Yaneva <alexandrayaneva@yahoo.com>
2019-05-05 21:00:31 +0100

    changing bootstrap theme

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask import render_template, url_for, request, redirect
+from config import API_key
 import requests
 import json
 
 
 app = Flask(__name__)
+base_url = 'https://api.themoviedb.org/3'
+images_url = 'http://image.tmdb.org/t/p/w185' #need to do this in config - a function to get the current images url
 
 
 @app.route('/')
@@ -16,14 +19,14 @@ def index():
 @app.route('/results', methods=['GET', 'POST'])
 def results():
 	title = request.form['title']
-	r = requests.get(f'http://www.omdbapi.com/?t={title}&apikey=b644d0fd')
+	r = requests.get(f'{base_url}/search/movie?api_key={API_key}&language=en-US&page=1&query={title}&include_adult=false')
 	json_obj = r.json()
 
-	title = json_obj['Title']
-	genre = json_obj['Genre']
-	cast = json_obj['Actors']
-	img = json_obj['Poster']
-	result = f'{title} {genre} {cast}'
+	film_title = json_obj['results'][0]['title']
+	summary = json_obj['results'][0]['overview']
+	poster_path = json_obj['results'][0]['poster_path']
+	img = f'{images_url}{poster_path}'
+	result = f'{film_title} {summary}'
 
 	return render_template('results.html', result=result, img=img)
 
