45dc3b7e7af5b226921814ee983ffe51fdb18546
Alex Yaneva <alexandrayaneva@yahoo.com>
2019-05-06 21:49:16 +0100

    changing the app structure

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 import os
+from dotenv import load_dotenv
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+base_dir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(base_dir, '.env'))
 
+API_key = os.environ.get('API_KEY')
 
-API_key = 'd0de405fdafad2d37be241901c212e7a'
+class Config():
+
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'han-shot-first'
+	MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+
