8305c069f21bf58051987b400b712f3edb4a4aae
Alex Yaneva <alexandrayaneva@yahoo.com>
2019-05-26 22:10:02 +0100

    restyling templates and adding a jubmotron

--- app/api.py
+++ app/api.py
@@ -3,7 +3,7 @@ import requests
 import json
 from config import API_key
 
-images_url = 'http://image.tmdb.org/t/p/w185' #w185 here is my choice of image size
+images_url = 'http://image.tmdb.org/t/p/w185' # w185 here is my choice of image size
 base_url = 'https://api.themoviedb.org/3'
 
 class API():
@@ -12,6 +12,7 @@ class API():
 		r = requests.get(f'{base_url}/search/movie?api_key={API_key}&language=en-US&page=1&query={title}&include_adult=false')
 		json_obj = r.json()
 
+		# there could be hundreds of results from a search, for the time being i'm just showing the first 12
 		results = {}
 		for film in range(0, 13):
 			film_title = json_obj['results'][film]['title']
@@ -19,7 +20,8 @@ class API():
 
 			img = f'{images_url}{poster_path}'
 
-			results[film_title] = img #adding each key-value pair to the results dict
+			# adding each key-value pair to the results dict
+			results[film_title] = img 
 
 		return results
 
