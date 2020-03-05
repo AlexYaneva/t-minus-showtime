e57ab2800f052cf322243b9af44a8d3a8d8daad1
Alex Yaneva <alexandrayaneva@yahoo.com>
2020-03-05 14:49:20 +0000

    fixed db id issue in user table

--- app/models.py
+++ app/models.py
@@ -2,42 +2,47 @@ from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+
 class User(UserMixin, db.Model):
-	user_id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(64), index=True, unique=True)
-	email = db.Column(db.String(120), index=True, unique=True)
-	password_hash = db.Column(db.String(128))
-	tracked_films = db.relationship('TrackedFilms', backref='user', lazy='dynamic')
-	tracked_series = db.relationship('TrackedSeries', backref='user', lazy='dynamic')
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    tracked_films = db.relationship("TrackedFilms", backref="user", lazy="dynamic")
+    tracked_series = db.relationship("TrackedSeries", backref="user", lazy="dynamic")
 
-	def __repr__(self):
-		return f'<User {self.username}>' 
+    def __repr__(self):
+        return f"<User {self.username}>"
 
-	def set_password(self, password):
-		self.password_hash = generate_password_hash(password)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-	def check_password(self, password):
-		return check_password_hash(self.password_hash, password)
-		
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def get_id(self):
+        return self.user_id
+
 
 @login.user_loader
 def load_user(user_id):
-	return User.query.get(int(user_id))
+    return User.query.get(int(user_id))
 
 
 class TrackedFilms(db.Model):
-	film_id = db.Column(db.Integer, primary_key=True)
-	film_title = db.Column(db.String(140), index=True)
-	user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    film_id = db.Column(db.Integer, primary_key=True)
+    film_title = db.Column(db.String(140), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.user_id"))
 
-	def __repr__(self):
-		return f'<TrackedFilm {self.film_title}'
+    def __repr__(self):
+        return f"<TrackedFilm {self.film_title}"
 
 
 class TrackedSeries(db.Model):
-	series_id = db.Column(db.Integer, primary_key=True)
-	series_title = db.Column(db.String(140), index=True)
-	user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    series_id = db.Column(db.Integer, primary_key=True)
+    series_title = db.Column(db.String(140), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.user_id"))
 
-	def __repr__(self):
-		return f'<TrackedSeries {self.series_title}'
+    def __repr__(self):
+        return f"<TrackedSeries {self.series_title}"
+
--- app/routes.py
+++ app/routes.py
@@ -6,65 +6,63 @@ from flask import render_template, url_for, request, redirect
 from flask_login import current_user, login_user, logout_user
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-	return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/results', methods=['GET', 'POST'])
+@app.route("/results", methods=["GET", "POST"])
 def results():
-	if request.form.get('film_title'):
-		film_title = request.form['film_title']
-		obj = API()
-		results = obj.search_film(film_title)
-	elif request.form.get('series_title'):
-		series_title = request.form['series_title']
-		obj = API()
-		results = obj.search_series(series_title)
+    if request.form.get("film_title"):
+        film_title = request.form["film_title"]
+        obj = API()
+        results = obj.search_film(film_title)
+    elif request.form.get("series_title"):
+        series_title = request.form["series_title"]
+        obj = API()
+        results = obj.search_series(series_title)
 
-	return render_template('results.html', results=results)
+    return render_template("results.html", results=results)
 
 
-@app.route('/films', methods=['GET', 'POST'])
+@app.route("/films", methods=["GET", "POST"])
 def films():
-	obj = API()
-	results = obj.popular_films()
-	return render_template('films.html', results=results)
+    obj = API()
+    results = obj.popular_films()
+    return render_template("films.html", results=results)
 
 
-@app.route('/tvseries', methods=['GET', 'POST'])
+@app.route("/tvseries", methods=["GET", "POST"])
 def tvseries():
-	obj = API()
-	results = obj.popular_series()
-	return render_template('tvseries.html', results=results)
+    obj = API()
+    results = obj.popular_series()
+    return render_template("tvseries.html", results=results)
 
 
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-	if current_user.is_authenticated:
-		return redirect(url_for('index'))
-	form = LoginForm()
-	if form.validate_on_submit():
-		user = User.query.filter_by(username=form.username.data).first()
-		if user is None or not user.check_password(form.password.data):
-			flash('Invalid username or password')
-			return redirect(url_for('login'))
-		login_user(user, remember=form.remember_me.data)
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            print("Invalid username or password")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
 
-		# # Storing the value of 'next' i.e. which page the user wanted to see but was prompted to log in first
-		# next_page = request.args.get('next') 
+        # # Storing the value of 'next' i.e. which page the user wanted to see but was prompted to log in first
+        # next_page = request.args.get('next')
 
-		# # The parse and netloc are security checks
-		# if not next_page or url_parse(next_page).netloc != '': 
-		# 	next_page = url_for('main.index')
-		return redirect(url_for('index'))
+        # # The parse and netloc are security checks
+        # if not next_page or url_parse(next_page).netloc != '':
+        # 	next_page = url_for('main.index')
+        return redirect(url_for("index"))
 
-	return render_template('login.html', title='Sign In', form=form)
+    return render_template("login.html", title="Sign In", form=form)
 
 
-@app.route('/logout', methods=['GET', 'POST'])
+@app.route("/logout", methods=["GET", "POST"])
 def logout():
-	logout_user()
-	return redirect(url_for('index'))
-	
+    logout_user()
+    return redirect(url_for("index"))
