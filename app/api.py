cde01429b86864495c311353633276401a1a2402
Alex Yaneva <alexandrayaneva@yahoo.com>
2022-06-23 14:01:46 +0100

    small fix

--- app/api.py
+++ app/api.py
@@ -10,16 +10,15 @@ class TMDB:
     APIKEY = f"?api_key={API_key}"
     IMAGES_URL = "http://image.tmdb.org/t/p/w342"
     LOGOS_URL = "http://image.tmdb.org/t/p/original"
-    BACKDROPS_URL = "http://image.tmdb.org/t/p/w780" 
+    BACKDROPS_URL = "http://image.tmdb.org/t/p/w780"
 
     def __init__(self, page):
         self.page = f"&page={page}"
-        self.base_url = "https://api.themoviedb.org/3" #  should be self.get_base_url().. or should this be a separate task?
+        # should be self.get_base_url().. or should this be a separate task?
+        self.base_url = "https://api.themoviedb.org/3"
         self.language = "&language=en-US"
 
-
     def _request(self, path, path2, item_id, query, append_to_response):
-
         ''' method to issue a single http request'''
 
         response = requests.get(
@@ -27,20 +26,15 @@ class TMDB:
         ).json()
         return response
 
-
-
     def _async_requests(self, path, list_of_ids, append_to_response):
-
         ''' method to issue multiple http requests asynchronously'''
 
         urls = []
         for i in list_of_ids:
             url = f"{self.base_url}{path}{i}{self.APIKEY}{self.language}{self.page}{append_to_response}"
             urls.append(url)
-            
+
         return async_get_multiple(list_of_ids, urls)
-
-
 
     def _process_json_response(self, response):
 
@@ -52,17 +46,17 @@ class TMDB:
 
             if "next_episode_to_air" in item and item["next_episode_to_air"] is not None:
                 if "air_date" in item["next_episode_to_air"] and item["next_episode_to_air"]["air_date"] is not None:
-                    item["next_episode_to_air"]['formatted_date'] = convert_date(item["next_episode_to_air"]['air_date'])
-
+                    item["next_episode_to_air"]['formatted_date'] = convert_date(
+                        item["next_episode_to_air"]['air_date'])
 
             # get release date by country based on current user's location
             if "release_dates" in item:
                 results_list = item["release_dates"]["results"]
-                country_dict = next((item for item in results_list if item["iso_3166_1"] == current_user.location["country_code"]), None)
+                country_dict = next(
+                    (item for item in results_list if item["iso_3166_1"] == current_user.location["country_code"]), None)
                 if country_dict:
-                    item['formatted_date'] = convert_date(country_dict["release_dates"][0]["release_date"])
-
-
+                    item['formatted_date'] = convert_date(
+                        country_dict["release_dates"][0]["release_date"])
 
             if "watch/providers" in item:
                 try:
@@ -75,11 +69,9 @@ class TMDB:
                         for i in item["watch/providers"]:
                             i['logo_path'] = f"{self.LOGOS_URL}{i['logo_path']}"
                     except KeyError:
-                        item["watch/providers"] = None 
+                        item["watch/providers"] = None
 
         return response
-
-
 
 
 class GetFilms(TMDB):
@@ -89,8 +81,8 @@ class GetFilms(TMDB):
         "popular_films": "/movie/popular",
         "film_details": "/movie/",
         "similar": "/similar",
-        "in_theatres" : "/movie/now_playing",
-        "top_rated" : "/movie/top_rated"
+        "in_theatres": "/movie/now_playing",
+        "top_rated": "/movie/top_rated"
     }
 
     def set_countdown(self, item):
@@ -100,66 +92,65 @@ class GetFilms(TMDB):
             countdwn = 1000
         return countdwn
 
-
     def popular_films(self):
         path = self.paths.get("popular_films")
-        response = self._request(path=path, path2="", item_id="", query="", append_to_response="")
+        response = self._request(
+            path=path, path2="", item_id="", query="", append_to_response="")
         response = response["results"]
         return self._process_json_response(response)
-
 
     def search_films(self, query):
         query = f"&query={query}"
         path = self.paths.get("search_film")
-        response = self._request(path=path, path2="", item_id="", query=query, append_to_response="")
+        response = self._request(
+            path=path, path2="", item_id="", query=query, append_to_response="")
         response = response["results"]
         return self._process_json_response(response)
-
 
     def film_details(self, item_id):
         path = self.paths.get("film_details")
         append_to_response = "&append_to_response=watch/providers,release_dates"
         response = []
-        tmdb_req = self._request(path=path, path2="", item_id=item_id, query="", append_to_response=append_to_response)
+        tmdb_req = self._request(
+            path=path, path2="", item_id=item_id, query="", append_to_response=append_to_response)
         response.append(tmdb_req)
-        return self._process_json_response(response) 
-
+        return self._process_json_response(response)
 
     def async_film_details(self, list_of_ids):
         path = self.paths.get("film_details")
         append_to_response = "&append_to_response=watch/providers,release_dates"
-        response = self._async_requests(path=path, list_of_ids=list_of_ids, append_to_response=append_to_response)
+        response = self._async_requests(
+            path=path, list_of_ids=list_of_ids, append_to_response=append_to_response)
         return self._process_json_response(response)
-
-
 
     def film_recommendations(self, item_id):
         path = self.paths.get("film_details")
         path2 = self.paths.get("similar")
-        response = self._request(path=path, path2=path2, item_id=item_id, query="", append_to_response="")
+        response = self._request(
+            path=path, path2=path2, item_id=item_id, query="", append_to_response="")
         response = response["results"]
         return self._process_json_response(response)
-
 
     def top_rated(self):
         path = self.paths.get("top_rated")
-        response = self._request(path=path, path2="", item_id="", query="", append_to_response="")
+        response = self._request(
+            path=path, path2="", item_id="", query="", append_to_response="")
         response = response["results"]
         return self._process_json_response(response)
 
-
     def films_in_theatres(self):
         path = self.paths.get("in_theatres")
-        response = self._request(path=path, path2="", item_id="", query="", append_to_response="")
+        response = self._request(
+            path=path, path2="", item_id="", query="", append_to_response="")
         response = response["results"]
         return self._process_json_response(response)
 
     def watch_providers(self, item_id):
         path = self.paths.get("film_details")
         path2 = "/watch/providers"
-        response = self._request(path=path, path2=path2, item_id=item_id, query="", append_to_response="")
+        response = self._request(
+            path=path, path2=path2, item_id=item_id, query="", append_to_response="")
         return response
-
 
 
 class GetSeries(TMDB):
@@ -181,62 +172,63 @@ class GetSeries(TMDB):
             countdwn = 1000
         return countdwn
 
-
     def popular_series(self):
         path = self.paths.get("popular_series")
-        response = self._request(path=path, path2="", item_id="", query="", append_to_response="")
+        response = self._request(
+            path=path, path2="", item_id="", query="", append_to_response="")
         response = response["results"]
         return self._process_json_response(response)
-
 
     def search_series(self, query):
         query += f"&query={query}"
         path = self.paths.get("search_series")
-        response = self._request(path=path, path2="", item_id="", query=query, append_to_response="")
+        response = self._request(
+            path=path, path2="", item_id="", query=query, append_to_response="")
         response = response["results"]
         return self._process_json_response(response)
-
 
     def series_details(self, item_id):
         path = self.paths.get("series_details")
         append_to_response = "&append_to_response=watch/providers,external_ids"
         response = []
-        tmdb_req = self._request(path=path, path2="", item_id=item_id, query="", append_to_response=append_to_response)
+        tmdb_req = self._request(
+            path=path, path2="", item_id=item_id, query="", append_to_response=append_to_response)
         response.append(tmdb_req)
         return self._process_json_response(response)
 
     def async_series_details(self, list_of_ids):
         path = self.paths.get("series_details")
         append_to_response = "&append_to_response=watch/providers"
-        response = self._async_requests(path=path, list_of_ids=list_of_ids, append_to_response=append_to_response)
+        response = self._async_requests(
+            path=path, list_of_ids=list_of_ids, append_to_response=append_to_response)
         return self._process_json_response(response)
-
 
     def series_recommendations(self, item_id):
         path = self.paths.get("series_details")
         path2 = self.paths.get("similar")
-        response = self._request(path=path, path2=path2, item_id=item_id, query="", append_to_response="")
+        response = self._request(
+            path=path, path2=path2, item_id=item_id, query="", append_to_response="")
         if "results" in response and response["results"] is not None:
             response = response["results"]
             return self._process_json_response(response)
 
-
     def series_airing_today(self):
         path = self.paths.get("airing_today")
-        response = self._request(path=path, path2="", item_id="", query="", append_to_response="")
+        response = self._request(
+            path=path, path2="", item_id="", query="", append_to_response="")
         response = response["results"]
         return self._process_json_response(response)
-
 
     def series_on_the_air(self):
         path = self.paths.get("on_the_air")
-        response = self._request(path=path, path2="", item_id="", query="", append_to_response="")
+        response = self._request(
+            path=path, path2="", item_id="", query="", append_to_response="")
         response = response["results"]
         return self._process_json_response(response)
-
 
     def watch_providers(self, item_id):
         path = self.paths.get("series_details")
         path2 = "/watch/providers"
-        response = self._request(path=path, path2=path2, item_id=item_id, query="", append_to_response="")
+        response = self._request(
+            path=path, path2=path2, item_id=item_id, query="", append_to_response="")
         return response
